3ff074c137bfeddc0f8ad675074599812bb777a4
j <san4ez131018@gmail.com>
2026-02-07 14:04:32 +0300

    hotfix for vercel

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('static'))
 app.use('/api', userRouter)
 
 module.exports = app
