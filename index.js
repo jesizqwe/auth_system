919c964cda6f774c8443688cb99a0bd75952a86f
j <san4ez131018@gmail.com>
2026-02-07 15:12:47 +0300

    hotfix for vercel

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('public')) 
+
 app.use('/api', userRouter)
 
 module.exports = app
 
-// if (require.main === module) {
-//     const PORT = process.env.PORT || 8080
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-// }
+if (require.main === module) {
+    const PORT = process.env.PORT || 8080
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+}
